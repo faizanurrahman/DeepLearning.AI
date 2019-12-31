ef345d9f367cc40bd002022932638bb72d209cb0
xman <faiza9ur@gmail.com>
2020-01-01 02:02:04 +0530

    Parameters initialized: Random, He, Xavier

---
+++ Deep_Neural_Network_Scratch/Initialize_Parameters/initialize_all_layer_parameters.py
@@ -0,0 +1,45 @@
+import numpy as np
+from Deep_Neural_Network_Scratch.Test_File import *
+
+
+def initialize_parameters_deep(layer_dims, init_type='random'):
+	"""
+	Arguments:
+	layer_dims -- list containing the dimensions of each layer in our network
+
+	Returns:
+	parameters -- dictionary containing parameters "W1", "b1", ..., "WL", "bL":
+					Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+					bl -- bias vector of shape (layer_dims[l], 1)
+	"""
+
+	np.random.seed(3)  # for testing purpus
+	parameters = {}
+	L = len(layer_dims)  # number of layers in the network
+
+	for l in range(1, L):
+		# initialize parameters for each layer
+		if init_type == 'random':
+			parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+			parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+		elif init_type == 'he':
+			parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * (np.sqrt(2/layer_dims[l-1]))
+			parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+		elif init_type == 'xavier':
+			parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * (np.sqrt(1/layer_dims[l - 1]))
+			parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+		else:
+			print("Please Check the parameter initialization type: ")
+		# check shape of parameters
+		assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+		assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+	return parameters
+
+
+if __name__ == '__main__':
+	parameters = initialize_parameters_deep([5, 4, 3], init_type='xavier')
+	print("W1 = " + str(parameters["W1"]))
+	print("b1 = " + str(parameters["b1"]))
+	print("W2 = " + str(parameters["W2"]))
+	print("b2 = " + str(parameters["b2"]))
